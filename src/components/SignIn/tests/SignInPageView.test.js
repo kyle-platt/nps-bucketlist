2509f9c0088decb4388c11b353a62345a29b11d5
Kyle Platt <kyleplatt2014@gmail.com>
2020-01-23 22:09:46 -0500

    Adding first snapshot test

---
+++ src/components/SignIn/tests/SignInPageView.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {BrowserRouter } from 'react-router-dom';
+import SignInPageView from '../SignInPageView';
+
+it('renders with some data', () => {
+  const tree = renderer.create(
+    <BrowserRouter>
+        <SignInPageView
+        email={'info@test.com'}
+        error={null}
+        handleChange={() => {}}
+        handleSubmit={() => {}}
+        isInvalid={false}
+        password={'123abc'}
+        />
+    </BrowserRouter> 
+    ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders with an error', () => {
+    const tree = renderer.create(
+    <BrowserRouter>
+      <SignInPageView
+      email={'info@test.com'}
+      error={{ message: 'some error' }}
+      handleChange={() => {}}
+      handleSubmit={() => {}}
+      isInvalid={true}
+      password={'123abc'}
+      />
+    </BrowserRouter>
+      ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
